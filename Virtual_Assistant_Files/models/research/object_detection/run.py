74f2228c878ae395638621f0bebd3811a870a7c6
Federico Maresca <62501138+Federico-Maresca@users.noreply.github.com>
2022-02-23 19:20:14 +0100

    Delete Virtual_Assistant_Files/models directory

--- Virtual_Assistant_Files/models/research/object_detection/run.py
+++
@@ -1,4 +0,0 @@
-python model_main_tf2.py --pipeline_config_path=training_detector\pipeline.config --model_dir=training_detector --alsologtostderr
-python exporter_main_v2.py --trained_checkpoint_dir=training_detector --pipeline_config_path=training_detector\pipeline.config --output_directory inference_graph 
-python generate_tfrecord.py --csv_input=images/train_labels.csv --image_dir=images/train --output_path=train.record
-python generate_tfrecord.py --csv_input=images/test_labels.csv --image_dir=images/test --output_path=test.record
