6c03dd627ce182c27364c7a3fceef33b124b78be
Federico-Maresca <fmare94@gmail.com>
2022-02-24 17:50:24 +0100

    new file:   setup.py 	deleted:    setup.sh

---
+++ setup.py
@@ -0,0 +1,12 @@
+import sys
+import os
+#Install github Python dependencies
+
+if sys.platform.startswith("linux") :
+  python = 'python3'
+elif sys.platform.startswith("win32") :
+  python = 'python'
+os.system(python + '-m pip install pip --upgrade')
+os.system(python + '-m pip install -r requirements.txt')
+
+print("Dependencies where installed, all ready to go!")
